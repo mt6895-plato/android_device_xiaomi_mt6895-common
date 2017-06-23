b94f9e29b3545fc42b5e2b1c216fe90cc6309fa5
Mathieu Chartier <mathieuc@google.com>
2017-06-23 12:59:56 -0700

    mt6895-common: Reduce system server verbosity
    Test: lunch cannon-user && m
    Change-Id: I58d2e993ae9a7213e94661d22b47c28b0a2243e8
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- mt6895.mk
+++ mt6895.mk
@@ -195,6 +195,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/rsc,$(TARGET_COPY_OUT_VENDOR)/etc/rsc)
 
+# Reduce system server verbosity.
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6895 \
