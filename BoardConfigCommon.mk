74e67d56144ab32274d2844aa358e549b627e675
J. Avila <elavila@google.com>
2021-04-12 18:22:36 +0000

    mt6895-common: Explicitly set log_buf_len
    By setting the log_buf_len via cmdline, devices using this cmdline will
    be able to guarantee their preferred buffer size even if the underlying
    CONFIG_LOG_BUF_SHIFT changes.
    
    Bug: 185141673
    Change-Id: Iafe7561dceef65fc47cb0bb54a8f84a5e10c51a9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,6 +56,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 bootconfig
 BOARD_KERNEL_CMDLINE += cgroup_disable=memory
+BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 BOARD_BOOTCONFIG += androidboot.serialconsole=0
 
 # Bootloader
