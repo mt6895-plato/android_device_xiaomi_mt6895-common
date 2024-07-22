f5e230689fb583a598ae4eff47157c0d6c368c8c
Mashopy <eliasgheeraert@gmail.com>
2024-07-22 21:45:01 +0700

    mt6895-common: Inherit QTI vibrator makefile directly
    Change-Id: I6fc62754791bed27dbfc51960139c8b5b42dd686

--- mt6895.mk
+++ mt6895.mk
@@ -324,11 +324,7 @@ PRODUCT_PACKAGES += \
 # Vibrator
 $(call soong_config_set_bool, vibrator, vibratortargets, vibratoraidlV2target)
 
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
