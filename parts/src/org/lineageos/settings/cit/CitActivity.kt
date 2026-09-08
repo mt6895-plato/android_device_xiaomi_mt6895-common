f9a0b60bf9869a1b3c97b2d3b123c5ee99c76b4d
archcloudy <cloudguy9@proton.me>
2026-09-09 03:32:30 +0700

    aahhh im crashing out

--- parts/src/org/lineageos/settings/cit/CitActivity.kt
+++ parts/src/org/lineageos/settings/cit/CitActivity.kt
@@ -1,33 +1,22 @@
 /*
- * SPDX-FileCopyrightText: 2024 The LineageOS Project
+ * SPDX-FileCopyrightText: The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.lineageos.settings.cit
+package org.lineageos.settings.citapp
 
 import android.app.Activity
-import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 
-class CitActivity : Activity() {
+class CitLauncherActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        try {
-            startActivity(Intent().apply {
-                component = ComponentName("com.miui.cit", "com.miui.cit.home.HomeActivity")
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            })
-        } catch (e: Exception) {
-            Log.e(TAG, "Failed to launch CIT", e)
-            Toast.makeText(this, "CIT app not found", Toast.LENGTH_SHORT).show()
-        }
+        startActivity(
+            Intent().setClassName("com.miui.cit", "com.miui.cit.home.HomeActivity")
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        )
         finish()
-    }
-
-    companion object {
-        private const val TAG = "CitActivity"
     }
 }
